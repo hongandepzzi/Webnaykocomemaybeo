c20fcdbb686f95da4bd1fe6cf9c4c0ccdab76620
hongandepzzi <94431987+hongandepzzi@users.noreply.github.com>
2023-08-09 15:20:50 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    alert("Bố mày gầy");
+  }
+function muFunction() {
+    alert("Me May Beo");
+}
+Swal.fire({
+    title: 'Thằng Ân nói',
+    text: 'Mẹ Mày béo vl ra ấy',
+    icon: 'info',
+    confirmButtonText: 'OK',
+  });
+function miFunction() {
+    alert("sao may beo vay tran=)))")
+}
